55046ca8d789a23b982ebcfbb893117b19c09051
Desnown <fandersonfh286@gmail.com>
2019-06-02 02:37:38 -0300

    Delete packages.py

--- DMusic/packages.py
+++
@@ -1,8 +0,0 @@
-from os import system
-packages = ['glob2', 'kivy', 'kivymd', 'python-vlc', 'termcolor']
-
-for pack in packages:
-    try:    
-        system(f'python3 -m pip install {pack} --user')
-    except:
-        pass
